ca8528058046e689f6000adff310e748e70a976d
Ian Brennan <ian@convertkit.com>
2024-09-07 09:12:41 -0600

    simplify server

--- app.js
+++ app.js
@@ -27,122 +27,86 @@ cloudinary.config({
   api_secret: cloudinarySecret,
 });
 
-function MessagingRouter() {
-  let twilioClient;
-  let images = [];
+async function SaveMedia(mediaItem) {
+  const { mediaSid, MessageSid, mediaUrl, extension, filename } = mediaItem;
+  // cloudinary picks up env and is now configured
+  console.log(cloudinary.config().cloud_name);
+  let resourceType = "image";
 
-  if (!fs.existsSync(PUBLIC_DIR)) {
-    fs.mkdirSync(path.resolve(PUBLIC_DIR));
+  if (
+    extension === ".mp4" ||
+    extension === ".m4a" ||
+    extension === ".f4v" ||
+    extension === ".m4b" ||
+    extension === ".mov"
+  ) {
+    resourceType = "video";
   }
 
-  function getTwilioClient() {
-    return twilioClient || new Twilio(twilioAccountSid, twilioAuthToken);
-  }
+  cloudinary.uploader
+    .upload(mediaUrl, {
+      // image is the default resource type if you don't specify
+      resource_type: resourceType,
+    })
+    .then((result) => {
+      // JSON.stringify will provide a formatted string
+      // 1st param is the value to be output
+      // 2nd param null is a function that can be applied to the output
+      // 3rd param is the number of space characters to use for whitespace in formatting the output
+      console.log("success", JSON.stringify(result, null, 2));
+    })
+    .catch((error) => {
+      console.log("error", JSON.stringify(error, null, 2));
+    });
+}
 
-  async function SaveMedia(mediaItem) {
-    const { mediaSid, MessageSid, mediaUrl, extension, filename } = mediaItem;
-    // cloudinary picks up env and is now configured
-    console.log(cloudinary.config().cloud_name);
-    let resourceType = "image";
+async function handleIncomingSMS(req, res) {
+  const { body } = req;
+  const { NumMedia, From: SenderNumber, MessageSid } = body;
+  // let saveOperations = [];
+  // const mediaItems = [];
 
-    if (
-      extension === ".mp4" ||
-      extension === ".m4a" ||
-      extension === ".f4v" ||
-      extension === ".m4b" ||
-      extension === ".mov"
-    ) {
-      resourceType = "video";
-    }
+  // for (var i = 0; i < NumMedia; i++) {
+  //   // eslint-disable-line
+  //   const mediaUrl = body[`MediaUrl${i}`];
+  //   const contentType = body[`MediaContentType${i}`];
+  //   const extension = extName.mime(contentType)[0].ext;
+  //   const mediaSid = path.basename(urlUtil.parse(mediaUrl).pathname);
+  //   const filename = `${mediaSid}.${extension}`;
 
-    cloudinary.uploader
-      .upload(mediaUrl, {
-        // image is the default resource type if you don't specify
-        resource_type: resourceType,
-      })
-      .then((result) => {
-        // JSON.stringify will provide a formatted string
-        // 1st param is the value to be output
-        // 2nd param null is a function that can be applied to the output
-        // 3rd param is the number of space characters to use for whitespace in formatting the output
-        console.log("success", JSON.stringify(result, null, 2));
-      })
-      .catch((error) => {
-        console.log("error", JSON.stringify(error, null, 2));
-      });
-  }
+  //   mediaItems.push({ mediaSid, MessageSid, mediaUrl, filename, extension });
+  //   saveOperations = mediaItems.map((mediaItem) => SaveMedia(mediaItem));
+  // }
 
-  async function handleIncomingSMS(req, res) {
-    const { body } = req;
-    const { NumMedia, From: SenderNumber, MessageSid } = body;
-    // let saveOperations = [];
-    // const mediaItems = [];
+  // await Promise.all(saveOperations);
 
-    // for (var i = 0; i < NumMedia; i++) {
-    //   // eslint-disable-line
-    //   const mediaUrl = body[`MediaUrl${i}`];
-    //   const contentType = body[`MediaContentType${i}`];
-    //   const extension = extName.mime(contentType)[0].ext;
-    //   const mediaSid = path.basename(urlUtil.parse(mediaUrl).pathname);
-    //   const filename = `${mediaSid}.${extension}`;
+  const messageBody =
+    NumMedia === 0
+      ? "Send us an image!"
+      : `Thanks for sending us ${NumMedia} file(s)`;
 
-    //   mediaItems.push({ mediaSid, MessageSid, mediaUrl, filename, extension });
-    //   saveOperations = mediaItems.map((mediaItem) => SaveMedia(mediaItem));
-    // }
+  const response = new MessagingResponse();
+  response.message(
+    {
+      from: twilioPhoneNumber,
+      to: SenderNumber,
+    },
+    messageBody
+  );
 
-    // await Promise.all(saveOperations);
-
-    const messageBody =
-      NumMedia === 0
-        ? "Send us an image!"
-        : `Thanks for sending us ${NumMedia} file(s)`;
-
-    const response = new MessagingResponse();
-    response.message(
-      {
-        from: twilioPhoneNumber,
-        to: SenderNumber,
-      },
-      messageBody
-    );
-
-    return res.send(response.toString()).status(200);
-  }
-
-  function getRecentImages() {
-    return images;
-  }
-
-  function clearRecentImages() {
-    images = [];
-  }
-
-  function fetchRecentImages(req, res) {
-    res.status(200).send(getRecentImages());
-    clearRecentImages();
-  }
-
-  /**
-   * Initialize router and define routes.
-   */
-  const router = express.Router();
-  router.post("/incoming", handleIncomingSMS);
-  router.get("/config", (req, res) => {
-    res.status(200).send({ twilioPhoneNumber });
-  });
-  router.get("/images", fetchRecentImages);
-
-  return router;
+  return res.send(response.toString()).status(200);
 }
 
 var app = express();
 
+app.get("/config", (req, res) => {
+  res.status(200).send({ hey: "hi" });
+});
+
+app.post("/incoming", handleIncomingSMS);
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use("/", MessagingRouter);
 
 module.exports = app;
